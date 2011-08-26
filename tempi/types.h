34180086508707c1652217c8a3bf89410892c933
Alexandre Quessy <alexandre@quessy.net>
2011-08-26 14:50:27 -0400

    add tempi/types.h

---
+++ tempi/types.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2011 Alexandre Quessy
+ * 
+ * This file is part of Tempi.
+ * 
+ * Tempi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Tempi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Tempi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __TEMPI_TYPES_H__
+#define __TEMPI_TYPES_H__
+
+#include <string>
+
+namespace tempi
+{
+
+namespace fsm
+{
+
+typedef float f;
+typedef float[2] ff;
+typedef float[3] fff;
+typedef float[4] ffff;
+typedef int i;
+typedef int[2] ii;
+typedef int[3] iii;
+typedef int[4] iiiii
+typedef std::string s;
+
+} // end of namespace
+
+} // end of namespace
+
+#endif // ifndef
+
