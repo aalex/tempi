dba1ec83a9fe2482e7d0d8a000fdf53ced9bce6d
Alexandre Quessy <alexandre@quessy.net>
2011-09-07 18:19:10 -0400

    update check_track.cpp

--- tests/check_track.cpp
+++ tests/check_track.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
-//#include "tempi/track.h"
+#include "tempi/tempi.h"
 
 using namespace tempi;
 
 bool check_simple()
 {
-    // TODO
+    Track track;
+    if (track.getDuration() != 0L)
+    {
+        std::cout << "Duration should be 0 ns." << std::endl;
+        return false;
+    }
+
+    track.add(1L, types::f(0.0f));
+    if (track.getDuration() != 1L)
+    {
+        std::cout << "Duration should be 1 ns." << std::endl;
+        return false;
+    }
+
+    track.add(2L, types::f(0.0f));
+    if (track.getDuration() != 2L)
+    {
+        std::cout << "Duration should be 2 ns." << std::endl;
+        return false;
+    }
+
+    if (track.numberOfEvents() != 2)
+    {
+        std::cout << "Should contain 2 events." << std::endl;
+        return false;
+    }
     return true;
 }
 
