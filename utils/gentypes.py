9816a5b75459e42ab65e4f75505bb51716d077a5
Alexandre Quessy <alexandre@quessy.net>
2011-09-07 19:02:30 -0400

    progress in utils/gentypes.py

--- utils/gentypes.py
+++ utils/gentypes.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Tempi.  If not, see <http://www.gnu.org/licenses/>.
 
-header = """
+HEADER = """
 /*
  * Copyright (C) 2011 Alexandre Quessy
  * 
@@ -42,6 +42,7 @@ header = """
 #define __TEMPI_TYPES_H__
 
 #include <string>
+#include <boost/any.hpp>
 #include <boost/tuple/tuple.hpp>
 
 namespace tempi
@@ -51,7 +52,7 @@ namespace types
 {
 """
 
-footer = """
+FOOTER = """
 
 } // end of namespace
 
@@ -60,25 +61,39 @@ footer = """
 #endif // ifndef
 """
 
-def get_one(prefix, suffix):
-    types = {
-        "i": "int",
-        "f": "float",
-        "d": "double",
-        "c": "char",
-        "b": "bool",
-        "s": "std::string",
-    }
-    return prefix + 
+TYPES = {
+    "i": "int",
+    "u": "unsigned int",
+    "f": "double", # or float?
+    "d": "double",
+    "c": "char",
+    "b": "bool",
+    "s": "std::string",
+    "a": "boost::any",
+}
 
 def print_types():
-    for first in types.iterkeys():
-        for depth in range(10):
-            for _type in types.iterkeys():
-                print("typedef boost::tuple<")
-
+    lines = []
+    for length_minus_one in range(5):
+        word_length = length_minus_one + 1
+        number_of_words_of_this_length = len(TYPES) ** word_length
+        for permutation_number in range(number_of_words_of_this_length):
+            prefix = "typedef boost::tuple<"
+            name = ""
+            for char_pos in range(word_length):
+                index = (permutation_number * char_pos) % len(TYPES)
+                # print "XXXXXXXXXXXXXXXXXXXXXXXXX", index
+                name += TYPES.keys()[index]
+                prefix += TYPES.values()[index]
+                if char_pos < (word_length - 1):
+                    prefix += ", "
+            prefix += ">"
+            lines.append("%100s %s;" % (prefix, name))
+    for line in lines:
+        print(line)
 
 if __name__ == "__main__":
-    print(header)
+    print(HEADER)
     print_types()
-    print(footer)
+    print(FOOTER)
+
