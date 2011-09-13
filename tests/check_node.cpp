01650b0c32627977d68060a3e7d046d32d615bdb
Alexandre Quessy <alexandre@quessy.net>
2011-09-12 22:03:42 -0400

    test filter node.

--- tests/check_node.cpp
+++ tests/check_node.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
+#include <boost/any.hpp>
 #include "tempi/tempi.h"
 #include "tempi/node.h"
 #include "tempi/sink.h"
 #include "tempi/source.h"
+#include "tempi/sourcenode.h"
+#include "tempi/sinknode.h"
+#include "tempi/filter.h"
 
 using namespace tempi;
 
 /**
  * Dummy source node.
  */
-class DummySourceNode: public Node
+class DummySourceNode: public SourceNode
 {
     public:
         DummySourceNode();
@@ -17,36 +21,63 @@ class DummySourceNode: public Node
         virtual void processMessage(Source *source, boost::any data);
 };
 
-DummySourceNode::DummySourceNode() : Node() {}
+DummySourceNode::DummySourceNode() : SourceNode() {}
 void DummySourceNode::processMessage(Source *source, boost::any data) {}
+
+/**
+ * Dummy filter node.
+ */
+class DummyFilterNode: public Filter
+{
+    public:
+        DummyFilterNode();
+    private:
+        /**
+         * Inverts the boolean value.
+         */
+        virtual boost::any filter(boost::any data);
+};
+
+DummyFilterNode::DummyFilterNode() : Filter() {}
+
+boost::any DummyFilterNode::filter(boost::any data)
+{
+    return boost::any(! boost::any_cast<bool>(data)); // pass through
+}
 
 /**
  * Dummy sink node.
  */
-class DummySinkNode: public Node
+class DummySinkNode: public SinkNode
 {
     public:
         DummySinkNode();
         bool triggered_;
+        bool last_value_was_ok_;
+        bool expected_;
     private:
         virtual void processMessage(Source *source, boost::any data);
 };
 
-DummySinkNode::DummySinkNode() : Node()
+DummySinkNode::DummySinkNode() :
+    SinkNode()
 {
     triggered_ = false;
+    expected_ = false;
+    last_value_was_ok_ = false;
 }
 
 void DummySinkNode::processMessage(Source *source, boost::any data)
 {
     triggered_ = true;
+    last_value_was_ok_ = boost::any_cast<bool>(data) == expected_;
 }
-
 
 bool check_simple()
 {
     DummySourceNode a;
-    DummySinkNode b;
+    DummyFilterNode b;
+    DummySinkNode c;
     if (! a.addOutlet(std::tr1::shared_ptr<Source>(new Source())))
     {
         std::cout << "Could not add outlet." << std::endl;
@@ -59,20 +90,31 @@ bool check_simple()
     }
     if (! b.getInlets()[0].get()->connect(a.getOutlets()[0]))
     {
-        std::cout << "Could not connect." << std::endl;
+        std::cout << "Could not connect a to b." << std::endl;
+        return false;
+    }
+    if (! c.getInlet()->connect(b.getOutlets()[0]))
+    {
+        std::cout << "Could not connect b to c." << std::endl;
         return false;
     }
     Source *source = a.getOutlets()[0].get();
     boost::any data(true);
-    if (b.triggered_)
+    if (c.triggered_)
     {
         std::cout << "Callback has already been called. Weird" << std::endl;
         return false;
     }
+    c.expected_ = false;
     source->trigger(data);
-    if (! b.triggered_)
+    if (! c.triggered_)
     {
         std::cout << "Callback has not been called." << std::endl;
+        return false;
+    }
+    if (! c.last_value_was_ok_)
+    {
+        std::cout << "Last value was not what was expected." << std::endl;
         return false;
     }
     return true;
