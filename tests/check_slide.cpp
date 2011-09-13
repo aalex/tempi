de87e2602d0c46a9b4cf1f6365364a7acf910e91
Alexandre Quessy <alexandre@quessy.net>
2011-09-12 23:52:11 -0400

    add Slide. reordered .gitignore

---
+++ tests/check_slide.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "tempi/tempi.h"
+#include "tempi/slide.h"
+
+using namespace tempi;
+
+bool check_slide()
+{
+    Slide slide = Slide();
+    slide.setSlide(1.0);
+    std::cout << "slide = " << slide.getSlide() << std::endl;
+    double factor = 1.0;
+    for (int i = 0; i < 100; ++i)
+    {
+        double result = slide.slide(i * factor);
+        std::cout << "" << i << " = " << result << std::endl;
+    }
+    return true;
+}
+
+int main(int argc, char *argv[])
+{
+    if (not check_slide())
+        return 1;
+    return 0;
+}
+
