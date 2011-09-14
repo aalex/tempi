c977eef061b770aab661e8776bd8cac448ad237f
Alexandre Quessy <alexandre@quessy.net>
2011-09-13 20:11:30 -0400

    animation looks a bit better in ease sine out

--- src/particlegenerator.cpp
+++ src/particlegenerator.cpp
@@ -35,19 +35,19 @@ void ParticleGenerator::onDraw()
     ClutterActor *particle = clutter_rectangle_new_with_color(&color);
     clutter_container_add_actor(CLUTTER_CONTAINER(group_), particle);
     clutter_actor_set_position(particle, source_x_, source_y_);
-    double size = g_rand_double_range(random_generator_, 1.0, 40.0);
-    gint duration = g_rand_int_range(random_generator_, 100, 1000);
+    double size = g_rand_double_range(random_generator_, 1.0, 20.0);
+    gint duration = g_rand_int_range(random_generator_, 500, 1500);
     clutter_actor_set_size(particle, size, size);
     clutter_actor_set_opacity(particle, 255);
     clutter_actor_set_anchor_point_from_gravity(particle, CLUTTER_GRAVITY_CENTER);
-    clutter_actor_set_z_rotation_from_gravity(particle, g_rand_double_range(random_generator_, -360.0, 360.0), CLUTTER_GRAVITY_CENTER);
-    ClutterAnimation *animation = clutter_actor_animate(particle, CLUTTER_LINEAR, duration, 
+    clutter_actor_set_z_rotation_from_gravity(particle, g_rand_double_range(random_generator_, -180.0, 180.0), CLUTTER_GRAVITY_CENTER);
+    ClutterAnimation *animation = clutter_actor_animate(particle, CLUTTER_EASE_OUT_QUAD, duration, 
         "x", (float) g_rand_double_range(random_generator_, source_x_ - 100.0, source_x_ + 100.0),
         "y", (float) g_rand_double_range(random_generator_, source_y_ - 100.0, source_y_ + 100.0),
         "width", 0.0f,
         "height", 0.0f,
         "opacity", 0,
-        "rotation-angle-z", g_rand_double_range(random_generator_, -360.0, 360.0),
+        "rotation-angle-z", g_rand_double_range(random_generator_, -180.0, 180.0),
         "signal::completed", on_animation_completed, particle,
         NULL);
 }
