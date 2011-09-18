bed12f3afc21c06363f91cc795bef241d2926a64
Alexandre Quessy <alexandre@quessy.net>
2011-09-18 13:18:24 -0400

    add python wrapper

---
+++ tempi/python-interface.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2011 Alexandre Quessy
+ * 
+ * This file is part of Tempi.
+ * 
+ * Tempi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Tempi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Tempi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "tempi/tempi.h"
+#include <boost/python.hpp>
+
+BOOST_PYTHON_MODULE(tempi)
+{
+    using namespace boost::python;
+    using namespace tempi;
+    def("hello", hello);
+    def("get_version", get_version);
+}
+
