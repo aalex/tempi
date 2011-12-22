aa7896982de391fc55427aaf5645d477eeea2203
Michal Seta <mis@artengine.ca>
2011-12-22 18:08:27 -0500

    Add [math.*]

--- tests/check_math.cpp
+++ tests/check_math.cpp
@@ -5,8 +5,8 @@
 #include <iostream>
 
 using namespace tempi;
-//static const bool VERBOSE = true;
-static const bool VERBOSE = false;
+static const bool VERBOSE = true;
+//static const bool VERBOSE = false;
 
 static bool check_add()
 {
@@ -87,7 +87,7 @@ static bool check_subtract()
 
     graph.tick(); // init the nodes
 
-    Message set_operand = Message("ssf", "set", "operand", 4.0f);
+    Message set_operand = Message("ssf", "set", "operand", 2.0f);
     graph.message("sub0", 0, set_operand);
     Message float_mess = Message("f", 2.0f);
     graph.message("sub0", 0, float_mess);
@@ -103,11 +103,62 @@ static bool check_subtract()
 
     return true;
 }
+
+static bool check_multiply()
+{
+    // Check we can instanciate it:
+    NodeFactory::ptr factory(new NodeFactory);
+    internals::loadInternals(factory);
+    Node::ptr add_obj = factory->create("math.*");
+    if (add_obj.get() == 0)
+    {
+        std::cout << "MultNode ptr is null" << std::endl;
+        return false;
+    }
+
+    // Check we can actually use it:
+    Graph graph(factory);
+    if (VERBOSE)
+        std::cout << "multiply nodes:" << std::endl;
+    graph.addNode("math.*", "mult0");
+    if (VERBOSE)
+        std::cout << "connect nodes:" << std::endl;
+    // [print]
+    if (VERBOSE)
+    {
+        graph.addNode("base.print", "print0");
+        graph.connect("mult0", 0, "print0", 0);
+    }
+    // TODO: store result in [any] or [appsink]
+    //graph.addNode("base.any", "any0");
+    //graph.connect("add0", 0, "any0", 0);
+
+    graph.tick(); // init the nodes
+
+    Message set_operand = Message("ssf", "set", "operand", 2.0f);
+    graph.message("mult0", 0, set_operand);
+    Message float_mess = Message("f", 2.0f);
+    graph.message("mult0", 0, float_mess);
+
+    // TODO when properties type will be dynamic and arguments deprecated
+    // float result = graph.getNode("any0").getProperty("value").getFloat(0);
+    // if ( != 4.0f)
+    // {
+    //     std::cout << "expected 4.0f in any0 but got "
+    // }
+
+    graph.tick();
+
+    return true;
+}
+
 int main(int argc, char **argv)
 {
     if (! check_add())
         return 1;
     if (! check_subtract())
+        return 1;
+    if (! check_multiply())
         return 1;
     return 0;
 
