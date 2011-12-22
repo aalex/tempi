8c0cf3c33b2e753ab0113adc23474a550598ebda
Alexandre Quessy <alexandre@quessy.net>
2011-12-22 16:24:34 -0500

    move around todos

--- tests/check_math.cpp
+++ tests/check_math.cpp
@@ -25,9 +25,11 @@ static bool check_add()
         std::cout << "add nodes:" << std::endl;
     graph.addNode("math.add", "add0");
     graph.addNode("base.print", "print0");
+    graph.addNode("base.any", "any0");
     if (VERBOSE)
         std::cout << "connect nodes:" << std::endl;
     graph.connect("add0", 0, "print0", 0);
+    graph.connect("add0", 0, "any0", 0);
 
     graph.tick(); // init the nodes
 
@@ -35,6 +37,13 @@ static bool check_add()
     graph.message("add0", 0, set_operand);
     Message float_mess = Message("f", 2.0f);
     graph.message("add0", 0, float_mess);
+
+    // TODO when properties type will be dynamic and arguments deprecated
+    // float result = graph.getNode("any0").getProperty("value").getFloat(0);
+    // if ( != 4.0f)
+    // {
+    //     std::cout << "expected 4.0f in any0 but got "
+    // }
 
     graph.tick();
 
