23b675f1fcc1daac19d813f9b4886a4cd8e37dc8
Alexandre Quessy <alexandre@quessy.net>
2012-02-07 10:14:50 -0500

    changed the look of tempi-inspect's output
    Might be still a bit ugly

--- src/tempi-inspect/main.cpp
+++ src/tempi-inspect/main.cpp
@@ -40,6 +40,22 @@ using namespace tempi;
 // String constants:
 static const char *PROGRAM_NAME = "tempi-inspect";
 
+// types:
+typedef enum
+{
+    FIRST = 1,
+    SECOND = 2,
+    THIRD = 3
+} TitleLevel;
+
+// static functions:
+static void print_n_times(unsigned int num, const char *text);
+static void print_title(const char *text, TitleLevel level);
+static void print_line_if_not_empty(const std::string &text);
+static void print_bullet_line_if_not_empty(const std::string &text);
+
+// classes:
+
 /**
  * The TempiInspect class helps navigating in Tempi's doc.
  */
@@ -57,11 +73,53 @@ class TempiInspect
         bool printClass(const std::string &name);
     private:
         bool verbose_;
+        bool all_;
         std::string keyword_;
         tempi::NodeFactory factory_;
         void printAll();
         void printListOfTypes();
 };
+
+static void print_title(const std::string &text, TitleLevel level)
+{
+    using std::cout;
+    using std::endl;
+    char character = '=';
+    switch (level)
+    {
+        case FIRST:
+            character = '=';
+            break;
+        case SECOND:
+            character = '-';
+            break;
+        case THIRD:
+            character = '~';
+            break;
+        default:
+            std::cerr << "Invalid title level" << std::endl;
+    }
+    cout << text << endl;
+    print_n_times(text.size(), std::string((const char) character).c_str());
+    cout << character;
+    cout << endl;
+    cout << endl;
+}
+
+static void print_line_if_not_empty(const std::string &text)
+{
+    if (text == "")
+        std::cout << "(none)" << std::endl;
+    else
+        std::cout << text << std::endl;
+}
+static void print_bullet_line_if_not_empty(const std::string &text)
+{
+    if (text == "")
+        std::cout << "* (none)" << std::endl;
+    else
+        std::cout << "* " << text << std::endl;
+}
 
 TempiInspect::TempiInspect() :
     verbose_(false)
@@ -96,68 +154,94 @@ bool TempiInspect::printClass(const std::string &name)
 
         // NODE TYPE NAME
         // ==============
-        cout << endl << endl;
+        //cout << endl << endl;
         cout << "Node Type [" << name << "]" << endl;
         cout << "===========";
         print_n_times(node->getTypeName().size(), "=");
-        cout << "=" << endl;
-        cout << endl;
-        cout << node->getShortDocumentation() << endl;
-        cout << endl;
+        cout << "=";
+        cout << endl; // after =========
+        cout << endl; // after =========
+        if (node->getShortDocumentation() != "")
+            cout << node->getShortDocumentation() << endl;
+        else
+            cout << "(Not documented)" << endl;
+        //cout << endl;
 
         // --------------------------------- Attributes ------------
         cout << "Attributes" << endl;
         cout << "----------" << endl;
         cout << endl;
         {
-            vector<string> attributes = node->listAttributes();
-            vector<string>::const_iterator iter;
-            for (iter = attributes.begin(); iter != attributes.end(); ++iter)
+            if (node->listAttributes().size() == 0)
+                cout << "(No attributes)" << endl;
+            else
             {
-                Attribute* attr = node->getAttribute((*iter).c_str());
-                cout << attr->getName() << endl;
-                print_n_times(attr->getName().size(), "~");
-                cout << endl;
-                cout << "Documentation: " <<  attr->getShortDocumentation() << endl;
-                if (attr->isTypeStrict())
-                    cout << "Type: " << attr->getValue().getTypes() << endl;
-                else
-                    cout << "Type: This attribute has a *variable type*." << endl;
-                cout << "Default value: " << attr->getValue() << endl;
-                cout << endl;
+                vector<string> attributes = node->listAttributes();
+                vector<string>::const_iterator iter;
+                for (iter = attributes.begin(); iter != attributes.end(); ++iter)
+                {
+                    Attribute* attr = node->getAttribute((*iter).c_str());
+                    cout << attr->getName() << endl;
+                    print_n_times(attr->getName().size(), "~");
+                    cout << endl; // after the ~~~~~
+                    if (attr->getShortDocumentation() != "")
+                        cout << attr->getShortDocumentation() << endl;
+                    else
+                        cout << "(Not documented)" << endl;
+                    if (attr->isTypeStrict())
+                        cout << "Type: " << attr->getValue().getTypes() << endl;
+                    else
+                        cout << "Type: This attribute has a *variable type*." << endl;
+                    cout << "Default value: " << attr->getValue() << endl;
+                    //cout << endl;
+                }
             }
         }
-        cout << endl;
+        //cout << endl;
         // --------------------------------- Inlets ------------
         cout << "Inlets" << endl;
         cout << "------" << endl;
-        cout << "All inlets::" << endl << endl;
+        cout << endl;
+        //cout << "All inlets::" << endl << endl;
+        if (node->getInlets().size() == 0)
+            cout << "(No inlet)" << endl;
+        else
         {
             map<string, Inlet::ptr> inlets = node->getInlets();
             map<string, Inlet::ptr>::const_iterator iter;
             for (iter = inlets.begin(); iter != inlets.end(); ++iter)
             {
                 cout << "* Inlet \"" << (*iter).first << "\":";
-                cout << ((*iter).second).get()->getDocumentation() << endl;
+                if (((*iter).second).get()->getDocumentation() != "")
+                    cout << ((*iter).second).get()->getDocumentation() << endl;
+                else
+                    cout << "(Not documented)" << endl;
             }
         }
-        cout << endl;
+        //cout << endl;
         // --------------------------------- Outlets ------------
         cout << "Outlets" << endl;
         cout << "------" << endl;
-        cout << "All outlets::" << endl << endl;
+        cout << endl;
+        //cout << "All outlets::" << endl << endl;
+        if (node->getOutlets().size() == 0)
+            cout << "(No outlet)" << endl;
+        else
         {
             map<string, Outlet::ptr> outlets = node->getOutlets();
             map<string, Outlet::ptr>::const_iterator iter;
             for (iter = outlets.begin(); iter != outlets.end(); ++iter)
             {
                 cout << "* Outlet \"" << (*iter).first << "\":";
-                cout << ((*iter).second).get()->getDocumentation() << endl;
+                if (((*iter).second).get()->getDocumentation() != "")
+                    cout << ((*iter).second).get()->getDocumentation() << endl;
+                else
+                    cout << "(Not documented)" << endl;
             }
         }
-        cout << endl;
+        //cout << endl;
         // -----------------------------------------------------
-        cout << endl;
+        //cout << endl;
     }
     else
         std::cerr << "No such node type: \"" << name << "\"" << std::endl;
@@ -165,46 +249,41 @@ bool TempiInspect::printClass(const std::string &name)
 
 void TempiInspect::printListOfTypes()
 {
-    std::cout << "Tempi types:" << std::endl;
-    std::cout << "============" << std::endl;
-    std::cout << std::endl;
-
+    print_title(std::string("Tempi types:", FIRST));
     std::map<std::string, AbstractNodeType::ptr>::const_iterator iter;
     std::map<std::string, AbstractNodeType::ptr> entries = factory_.getEntries();
     for (iter = entries.begin(); iter != entries.end(); ++iter)
     {
-        std::cout << "* " << (*iter).first << std::endl;
+        print_bullet_line_if_not_empty((*iter).first);
     }
 }
 
 void TempiInspect::printAll()
 {
-    std::cout << "Tempi types:" << std::endl;
-    std::cout << "============" << std::endl;
-    std::cout << std::endl;
-
+    print_title(std::string("Tempi types:", FIRST));
     std::map<std::string, AbstractNodeType::ptr>::const_iterator iter;
     std::map<std::string, AbstractNodeType::ptr> entries = factory_.getEntries();
     for (iter = entries.begin(); iter != entries.end(); ++iter)
     {
-        std::cout << (*iter).first << std::endl;
-        std::cout << "-------------------------------" << std::endl;
         printClass((*iter).first);
     }
 }
 
 bool TempiInspect::run()
 {
-    if (keyword_ == "")
-    {
-        printListOfTypes();
+    if (all_)
         printAll();
-    }
     else
     {
-        printClass(keyword_);
+        if (keyword_ == "")
+        {
+            printListOfTypes();
+        }
+        else
+        {
+            printClass(keyword_);
+        }
     }
-
     return true;
 }
 
@@ -216,6 +295,7 @@ int TempiInspect::parse_options(int argc, char **argv)
         ("version", "Show program's version number and exit")
         ("keyword,k", po::value<std::string>()->default_value(""), "Sets the keyword to look for in Tempi's documentation")
         ("verbose,v", po::bool_switch(), "Enables a verbose output")
+        ("all,a", po::bool_switch(), "Prints detailed info for all node types")
         ;
     po::variables_map options;
     try
@@ -235,6 +315,7 @@ int TempiInspect::parse_options(int argc, char **argv)
     }
 
     verbose_ = options["verbose"].as<bool>();
+    all_ = options["all"].as<bool>();
     keyword_ = options["keyword"].as<std::string>();
     // Options that makes the program exit:
     if (options.count("help"))
