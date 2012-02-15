5eb90b49a285567447479d9d52ec7544a5785712
Alexandre Quessy <alexandre@quessy.net>
2012-02-15 17:28:58 -0500

    create a Logger class that rocks

--- prototypes/log/main.cpp
+++ prototypes/log/main.cpp
@@ -1,28 +1,107 @@
 #include <iostream>
-#include <tr1/memory>
 #include <log4cpp/Category.hh>
-#include <log4cpp/FileAppender.hh>
 #include <log4cpp/OstreamAppender.hh>
-#include <log4cpp/SimpleLayout.hh>
- 
-static const char * const LOGFILE = "/tmp/log4cpp-test.log";
- 
+#include <log4cpp/PatternLayout.hh>
+
+namespace log {
+
+/**
+ * Log level
+ */
+typedef enum
+{
+    DEBUG = log4cpp::Priority::DEBUG,
+    INFO = log4cpp::Priority::INFO,
+    WARNING = log4cpp::Priority::WARN,
+    CRITICAL = log4cpp::Priority::CRIT,
+    ERROR = log4cpp::Priority::ERROR
+} LogLevel;
+
+class Logger
+{
+    public:
+        /**
+         * Retrieves the singleton.
+         */
+        static Logger& getInstance()
+        {
+            static Logger instance; // Guaranteed to be destroyed.
+            return instance; // Instantiated on first use.
+        }
+        /**
+         * Sets log level.
+         */
+        void setLevel(LogLevel level)
+        {
+            log4cpp::Category& category = log4cpp::Category::getInstance(category_name_);
+            category.setPriority(level);
+        }
+        /**
+         * Logs a message.
+         */
+        log4cpp::Category& log(LogLevel level, const char *message)
+        {
+            log4cpp::Category& category = log4cpp::Category::getInstance(category_name_);
+            switch (level)
+            {
+                case DEBUG:
+                    category.debug(message);
+                    break;
+                case INFO:
+                    category.info(message);
+                    break;
+                case WARNING:
+                    category.warn(message);
+                    break;
+                case CRITICAL:
+                    category.crit(message);
+                    break;
+                case ERROR:
+                    category.error(message);
+                    break;
+            }
+        }
+        ~Logger()
+        {
+            // No need to delete appender_ and layout_
+        }
+    private:
+        log4cpp::Appender *appender_;
+        log4cpp::PatternLayout *layout_;
+        static const char * const category_name_;
+
+        // Private constructor
+        Logger()
+        {
+            appender_ = new log4cpp::OstreamAppender("console", &std::cout);
+            layout_ = new log4cpp::PatternLayout();
+            layout_->setConversionPattern("%d: %8p - %m %n");
+            appender_->setLayout(layout_);
+            log4cpp::Category& category = log4cpp::Category::getInstance(category_name_);
+            category.setAppender(appender_);
+            category.setPriority(WARNING); //default
+        }
+        // Dont forget to declare these two. You want to make sure they
+        // are unaccessable otherwise you may accidently get copies of
+        // your singelton appearing.
+        Logger(Logger const&);         // Don't Implement
+        void operator=(Logger const&); // Don't implement
+};
+
+const char * const Logger::category_name_ = "default";
+
+} // end of namespace
+
 int main(int argc, char *argv[])
 {
-    // Setting up Appender, layout and Category
-    // log4cpp::Appender *appender = new log4cpp::FileAppender("logfile-manager", LOGFILE);
-    log4cpp::Appender *appender = new log4cpp::OstreamAppender("OstreamAppender", &std::cout);
-    log4cpp::Layout *layout = new log4cpp::SimpleLayout();
-    log4cpp::Category& category = log4cpp::Category::getInstance("hello");
- 
-    appender->setLayout(layout);
-    category.setAppender(appender);
-    category.setPriority(log4cpp::Priority::WARN); // INFO);
- 
-    // The actual logging
-    category.info("This is for tracing the flow");
-    category.notice("This is to notify certain events");
-    category.warn("This is to generate certain warnings");
+    using namespace log;
+    Logger& logger = Logger::getInstance();
+    logger.setLevel(INFO);
+    logger.log(DEBUG, "DEBUG");
+    logger.log(INFO, "INFO");
+    logger.log(WARNING, "WARNING");
+    logger.log(CRITICAL, "CRITICAL");
+    logger.log(ERROR, "ERROR");
     return 0;
 }
 
