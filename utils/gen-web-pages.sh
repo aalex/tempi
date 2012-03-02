1a6aea5a9ea142259acf645dc28507acb970f954
Alexandre Quessy <alexandre@quessy.net>
2012-03-01 19:06:49 -0500

    add css files for rst2html

--- utils/gen-web-pages.sh
+++ utils/gen-web-pages.sh
@@ -2,12 +2,15 @@
 cd $(dirname $0)
 cd ..
 HTML_DIR=doc/html
+RST2HTML_OPTS="--stylesheet voidspace.css --link-stylesheet"
 mkdir -p ${HTML_DIR}
-rst2html NEWS > ${HTML_DIR}/news.html
-rst2html README > ${HTML_DIR}/readme.html
-rst2html doc/index.rst > ${HTML_DIR}/index.html
-src/tempi-inspect/tempi-inspect -a | rst2html > ${HTML_DIR}/nodes.html
-make html
-mkdir -p ${HTML_DIR}/doxygen
-cp html/* ${HTML_DIR}/doxygen/
+rst2html ${RST2HTML_OPTS} NEWS > ${HTML_DIR}/news.html
+rst2html ${RST2HTML_OPTS} README > ${HTML_DIR}/readme.html
+rst2html ${RST2HTML_OPTS} doc/index.rst > ${HTML_DIR}/index.html
+src/tempi-inspect/tempi-inspect -a | rst2html ${RST2HTML_OPTS} > ${HTML_DIR}/nodes.html
+
+# make html
+# mkdir -p ${HTML_DIR}/doxygen
+# cp html/* ${HTML_DIR}/doxygen/
+
 
