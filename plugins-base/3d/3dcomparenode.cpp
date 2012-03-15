2905f1207dee31a5ff260a9b49499271a0532ea4
Alexandre Quessy <alexandre@quessy.net>
2012-03-15 14:11:28 -0400

    add Angle3dNode

---
+++ plugins-base/3d/3dcomparenode.cpp
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2011 Alexandre Quessy
+ * Copyright (C) 2011 Michal Seta
+ * Copyright (C) 2012 Nicolas Bouillot
+ *
+ * This file is part of Tempi.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Tempi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Tempi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "tempi/config.h" // include before check for HAVE_SPATOSC
+
+#ifdef HAVE_SPATOSC
+#include "plugins-base/3d/3dcomparenode.h"
+#include <iostream>
+
+namespace tempi { 
+namespace plugins_base {
+
+const char * const Abstract3dCompareNode::HOT_INLET = "0-hot";
+const char * const Abstract3dCompareNode::COLD_INLET = "1-cold";
+const char * const Abstract3dCompareNode::OPERAND_ATTR = "operand";
+
+Abstract3dCompareNode::Abstract3dCompareNode() :
+    Node()
+{
+    addInlet(HOT_INLET, "Triggers computation. Incoming left 3-float message operand. (hot)");
+    addInlet(COLD_INLET, "Stores operand. Incoming right 3-float message operand. (cold)");
+
+    addAttribute(Attribute::ptr(new Attribute(OPERAND_ATTR, Message("fff", 0.0f, 0.0f, 0.0f),
+        "Right operand to apply calculation with incoming 3-float messages.")));
+}
+
+void Abstract3dCompareNode::processMessage(const char *inlet, const Message &message)
+{
+    if (utils::stringsMatch(inlet, COLD_INLET))
+    {
+        if (message.typesMatch("fff"))
+            this->setAttributeValue(OPERAND_ATTR, message);
+        else
+        {
+            std::cerr << "Abstract3dCompareNode::" << __FUNCTION__ <<
+                "(): Bad type for message " << message << std::endl;
+            return;
+        }
+    }
+    else if (utils::stringsMatch(inlet, HOT_INLET))
+    {
+        if (message.typesMatch("fff"))
+        {
+            spatosc::Vector3 left_operand(message.getFloat(0), message.getFloat(1), message.getFloat(2));
+            Message right = getAttributeValue(OPERAND_ATTR);
+            spatosc::Vector3 right_operand(right.getFloat(0), right.getFloat(1), right.getFloat(2));
+            this->compare(left_operand, right_operand);
+        }
+        else
+        {
+            std::cerr << "Abstract3dCompareNode::" << __FUNCTION__ <<
+                "(): Bad type for message " << message << std::endl;
+        }
+    }
+}
+
+
+const char * const Angle3dNode::ANGLE_ELEVATION_OUTLET = "angle_elevation";
+
+Angle3dNode::Angle3dNode() :
+    Abstract3dCompareNode()
+{
+    setShortDocumentation("Outputs the angle and elevation between two 3-float XYZ vectors.");
+}
+
+void Angle3dNode::compare(const spatosc::Vector3 &left, const spatosc::Vector3 &right)
+{
+    double angle = spatosc::angleBetweenVectors(left, right, 2); // Y-axis is null
+    double elevation = spatosc::angleBetweenVectors(left, right, 1); // X-axis is null
+    Message ret("ff", (float) angle, (float) elevation);
+    this->output(ANGLE_ELEVATION_OUTLET, ret);
+}
+
+} // end of namespace
+} // end of namespace
+
+#endif // HAVE_SPATOSC
+
---
+++ plugins-base/3d/3dcomparenode.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2011 Alexandre Quessy
+ * Copyright (C) 2011 Michal Seta
+ * Copyright (C) 2012 Nicolas Bouillot
+ *
+ * This file is part of Tempi.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Tempi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Tempi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ * Math nodes that take two floats.
+ */
+#ifndef __TEMPI_3D_COMPARE_NODE_H__
+#define __TEMPI_3D_COMPARE_NODE_H__
+
+#include "tempi/config.h" // include before check for HAVE_SPATOSC
+
+#ifdef HAVE_SPATOSC
+#include "tempi/message.h"
+#include <spatosc/maths.h>
+#include "tempi/node.h"
+#include "tempi/utils.h"
+
+namespace tempi { 
+namespace plugins_base { 
+
+/**
+ * Abstract3dCompareNode is an abstract class for nodes that accept two 3-float vectors.
+ */
+class Abstract3dCompareNode : public Node
+{
+    public:
+        Abstract3dCompareNode();
+    protected:
+        virtual void processMessage(const char *inlet, const Message &message);
+    private:
+        virtual void compare(const spatosc::Vector3 &left, const spatosc::Vector3 &right);
+        static const char * const HOT_INLET;
+        static const char * const COLD_INLET;
+        static const char * const OPERAND_ATTR;
+};
+
+/**
+ * Ouputs the angle and elevation between two 3d-float messages.
+ */
+class Angle3dNode : public Abstract3dCompareNode
+{
+    public:
+        Angle3dNode();
+    private:
+        virtual void compare(const spatosc::Vector3 &left, const spatosc::Vector3 &right);
+        static const char * const ANGLE_ELEVATION_OUTLET;
+};
+
+} // end of namespace
+} // end of namespace
+
+#endif // HAVE_SPATOSC
+#endif // ifndef
+
