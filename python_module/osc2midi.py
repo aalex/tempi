16febb6bfb98f89413f4ffdbfc60bb3d3dda65b8
Michal Seta <mis@artengine.ca>
2012-03-21 14:54:00 -0400

    forward osc to MIDI

--- python_module/osc2midi.py
+++ python_module/osc2midi.py
@@ -9,21 +9,12 @@ def run():
 
     # ROUTE OSC
     g.node("osc.route", "route_midi")
-    g.attr("route_midi", "paths", tempy.mess("/note", "/ctl", "/program"))
+    g.attr("route_midi", "paths", tempy.mess("/note", "/control", "/program"))
     g.connect("oscreceive0", "incoming", "route_midi", "incoming")    
     
     # MIDINOTE
     g.node("midi.note", "note0")
     g.connect("route_midi", "/note", "note0", "ints")
-    
-    # MIDICTL
-    g.node("midi.control", "ctl0")
-    g.connect("route_midi", "/ctl", "ctl0", "ints")
-
-    # PRINT MIDICTL
-    g.node("base.print", "print_ctl")
-    g.attr("print_ctl", "prefix", tempy.mess("Got control:"))
-    g.connect("ctl0", "midi_event", "print_ctl", "0")
 
     # PRINT MIDINOTE
     g.node("base.print", "print_note")
