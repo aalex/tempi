8db164e2c1adb6418a3e7f82f7b16c338906cf0b
Alexandre Quessy <alexandre@quessy.net>
2012-07-29 12:13:23 -0400

    proto: split into many files

---
+++ prototypes/atomclass/intatom.h
@@ -0,0 +1,40 @@
+#ifndef __INT_ATOM_H__
+#define __INT_ATOM_H__
+
+#include "atom.h"
+#include <boost/lexical_cast.hpp>
+#include <sstream>
+
+class IntAtom: public Atom
+{
+    public:
+        IntAtom() {}
+        IntAtom(int value) :
+            value_(value)
+        {}
+        void setInt(int value)
+        {
+            value_ = value;
+        }
+        int getInt()
+        {
+            return value_;
+        }
+    private:
+        int value_;
+        virtual char doGetName()
+        {
+            return 'i';
+        }
+        virtual void doToJSON(std::string &result)
+        {
+            std::ostringstream os;
+            os << value_;
+            result = os.str();
+        }
+        virtual void doFromJSON(const char *json)
+        {
+            value_ = boost::lexical_cast<int>(json);
+        }
+};
+#endif
--- prototypes/atomclass/main.cpp
+++ prototypes/atomclass/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include <sstream>
 #include <tr1/memory>
 #include <vector>
-#include <boost/lexical_cast.hpp>
+#include "atom.h"
+#include "intatom.h"
+#include "listatom.h"
 
 /**
  * It would make more sense to refactor the message class so that the
@@ -22,130 +23,8 @@
  *  - dict
  */
 
-class Atom
-{
-    public:
-        typedef std::tr1::shared_ptr<Atom> ptr;
-        char getName();
-        void toJSON(std::string &result);
-        void fromJSON(const char *json);
-    private:
-        virtual char doGetName() = 0;
-        virtual void doToJSON(std::string &result) = 0;
-        virtual void doFromJSON(const char *json) = 0;
-};
 
-char Atom::getName()
-{
-    return this->doGetName();
-}
 
-void Atom::toJSON(std::string &result)
-{
-    this->doToJSON(result);
-}
-
-void Atom::fromJSON(const char *json)
-{
-    this->doFromJSON(json);
-}
-
-class IntAtom: public Atom
-{
-    public:
-        IntAtom() {}
-        IntAtom(int value) :
-            value_(value)
-        {}
-        void setInt(int value)
-        {
-            value_ = value;
-        }
-        int getInt()
-        {
-            return value_;
-        }
-    private:
-        int value_;
-        virtual char doGetName()
-        {
-            return 'i';
-        }
-        virtual void doToJSON(std::string &result)
-        {
-            std::ostringstream os;
-            os << value_;
-            result = os.str();
-        }
-        virtual void doFromJSON(const char *json)
-        {
-            value_ = boost::lexical_cast<int>(json);
-        }
-};
-
-class ListAtom: public Atom
-{
-    public:
-        ListAtom() {}
-        void append(Atom::ptr value)
-        {
-            atoms_.push_back(value);
-        }
-        void clear()
-        {
-            atoms_.clear();
-        }
-        bool set(unsigned int index, Atom::ptr value)
-        {
-            // TODO: check for bad index
-            atoms_[index] = value;
-            return true;
-        }
-        Atom::ptr get(unsigned int index)
-        {
-            // TODO: check for bad index
-            return atoms_[index];
-        }
-        unsigned int size()
-        {
-            return atoms_.size();
-        }
-    private:
-        std::vector<Atom::ptr> atoms_;
-        virtual char doGetName()
-        {
-            return 'l';
-        }
-        virtual void doToJSON(std::string &result)
-        {
-            std::ostringstream os;
-            std::vector<Atom::ptr>::const_iterator iter;
-            os << "[";
-            for (iter = atoms_.begin(); iter != atoms_.end(); iter++)
-            {
-                std::string jsonized;
-                (*iter)->toJSON(jsonized);
-                os << jsonized;
-                os << ",";
-            }
-            os << "]";
-            result = os.str();
-        }
-        virtual void doFromJSON(const char *json)
-        {
-            // TODO!
-            std::cout << __FUNCTION__ << ": not implemented.\n";
-        }
-};
-
-/**
- * Casts a Atom::ptr into a ListAtom*.
- * TODO: use shared_ptr casters instead.
- */
-ListAtom *LIST_ATOM(Atom::ptr atom)
-{
-    return static_cast<ListAtom*>(atom.get());
-}
 
 int main(int argc, char *argv[])
 {
