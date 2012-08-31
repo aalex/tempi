758dba6a575b037e0f0efb1848a7624225882e0b
Alexandre Quessy <alexandre@quessy.net>
2012-08-31 17:16:07 -0400

    xmlrpc prototype contains a metro that can be controlled

--- prototypes/xmlrpc-c3/build.sh
+++ prototypes/xmlrpc-c3/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-LIBS=`xmlrpc-c-config c++2 abyss-server client --libs --cflags`
-# -lxmlrpc_cpp -lxmlrpc_server++ -lxmlrpc_server_abyss++
-g++ -o server server.cpp $LIBS -lpthread
-g++ -o client client.cpp $LIBS -lpthread
+CLIENTLIBS=`xmlrpc-c-config c++2 client --libs --cflags`
+SERVERLIBS=`xmlrpc-c-config c++2 abyss-server --libs --cflags`
+TEMPILIBS=`pkg-config --libs --cflags tempi-0.1`
+
+g++ -o server server.cpp $SERVERLIBS $TEMPILIBS -lpthread
+g++ -o client client.cpp $CLIENTLIBS $TEMPILIBS -lpthread
 
