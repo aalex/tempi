1e634a63b77415f9fe283ca2365e90f4a53429fe
Alexandre Quessy <alexandre@quessy.net>
2012-08-31 15:51:11 -0400

    fix build script for xmlrpc proto

--- prototypes/xmlrpc-c3/build.sh
+++ prototypes/xmlrpc-c3/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-g++ -lxmlrpc_cpp -lxmlrpc_server++ -lxmlrpc_server_abyss++ -lpthread -o server server.cpp
-g++ -lxmlrpc_cpp -lxmlrpc_client++ -lpthread -o client client.cpp
+LIBS=`xmlrpc-c-config c++2 abyss-server client --libs --cflags`
+# -lxmlrpc_cpp -lxmlrpc_server++ -lxmlrpc_server_abyss++
+g++ -o server server.cpp $LIBS -lpthread
+g++ -o client client.cpp $LIBS -lpthread
 
