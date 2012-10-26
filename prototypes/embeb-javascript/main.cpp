d0d7d717fef62e9c14c137c4e589c27e29c84f64
Alexandre Quessy <alexandre@quessy.net>
2012-10-26 14:52:47 -0400

    javascript: handle some exceptions

--- prototypes/embeb-javascript/main.cpp
+++ prototypes/embeb-javascript/main.cpp
@@ -2,8 +2,18 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <stdexcept>
+
+class JavaScriptError : public std::runtime_error
+{
+    public:
+        JavaScriptError(const char *message) :
+            std::runtime_error(message)
+        {}
+};
 
 double calculate(const char *formula, const std::vector<double> &args)
+    throw(JavaScriptError)
 {
     v8::HandleScope scope;
     v8::Persistent<v8::Context> context = v8::Context::New();
@@ -17,12 +27,33 @@ double calculate(const char *formula, const std::vector<double> &args)
     code << formula << ";" << std::endl;
 
     std::cout << "-----------------\n";
-    std::cout << "CODE: \n" << code.str().c_str();
+    std::cout << "CODE: " << std::endl;
+    std::cout << code.str().c_str();
     std::cout << "-----------------\n";
 
     v8::Handle<v8::String> source = v8::String::New(code.str().c_str());
     v8::Handle<v8::Script> script = v8::Script::Compile(source);
+    v8::TryCatch try_catch;
+    // // handle exception:
+    // {
+    //     //v8::Handle<v8::Message> message = try_catch.Message();
+    //     std::ostringstream os;
+    //     os << __FUNCTION__ << ": Could not compile script \"" << formula; //  << "\": " << (* message.Get());
+    //     throw (JavaScriptError(os.str().c_str()));
+    // }
+    if (script.IsEmpty())
+    {
+        std::ostringstream os;
+        os << __FUNCTION__ << ": Could not compile script \"" << formula << "\"";
+        throw (JavaScriptError(os.str().c_str()));
+    }
     v8::Handle<v8::Value> result = script->Run();
+    if (! result->IsNumber())
+    {
+        std::ostringstream os;
+        os << __FUNCTION__ << ": Result is not a number: \"" << formula << "\"";
+        throw (JavaScriptError(os.str().c_str()));
+    }
 
     v8::Number *number = v8::Number::Cast(*result);
 
@@ -36,7 +67,17 @@ bool check_expr()
     std::vector<double> args;
     args.push_back(2.0);
     args.push_back(10.0);
-    std::cout << calculate("Math.pow(f0, f1)", args);
+    args.push_back(2.0);
+    try
+    {
+        double result = calculate("Math.pow(f0, f1)", args);
+        std::cout << result << std::endl;
+    }
+    catch (const JavaScriptError &e)
+    {
+        std::cerr << e.what() << std::endl;
+        return false;
+    }
     return true;
 }
 
