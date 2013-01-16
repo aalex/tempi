e349def093aa6fb50ef6709d0129efdcacc439aa
Alexandre Quessy <alexandre@quessy.net>
2013-01-16 01:16:00 -0500

    add base.serial.device node type

---
+++ plugins-base/serial/serialdevice.cpp
@@ -0,0 +1,224 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <tempi/timer.h>
+#include <iostream>
+#include "plugins-base/serial/serialdevice.h"
+#include "tempi/log.h"
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/thread/thread.hpp>
+
+using namespace tempi;
+
+bool SerialDevice::writeBlob(const char* data, size_t length)
+{
+    size_t written = write(fd_, data, length);
+    if (written != length) 
+        return false;
+    return true;
+}
+
+void SerialDevice::sleep_ms(unsigned long long ms) const
+{
+    //usleep(ms * 1000);
+    boost::posix_time::milliseconds sleepTime((double) ms);
+    boost::this_thread::sleep(sleepTime);
+}
+
+bool SerialDevice::readUntil(char *result, size_t max_length, size_t &total_num_read, unsigned long long timeout_ms, char until_char, bool use_until_char)
+{
+    bool success = true;
+    tempi::Timer timer;
+    unsigned long long elapsed;
+    char b[1];
+    size_t num_read = 0;
+    while (true)
+    {
+        if (num_read == max_length)
+            break;
+        int n = read(fd_, b, 1); // read a char at a time
+        if (n == -1) // error. TODO: see errno
+        {
+            //std::cout << __FUNCTION__ << ": Error!";
+            //std::cout << std::endl;
+            //success = false;
+            //break;
+            this->sleep_ms(sleep_time_between_each_char_); // wait 5 msec try again
+        }
+        else if (n == 0) // end of file
+        {
+            this->sleep_ms(sleep_time_between_each_char_); // wait 5 msec try again
+        }
+        else
+        {
+            if (use_until_char && b[0] == until_char)
+            {
+                result[num_read] = b[0]; // = 0;
+                break;
+            }
+            else
+            {
+                result[num_read] = b[0];
+                num_read++;
+            }
+        }
+        // check if timeout is reached
+        elapsed = tempi::timeposition::to_ms(timer.elapsed());
+        if (elapsed > timeout_ms)
+        {
+            break;
+        }
+    }
+    //if (num_read < max_length)
+    //    buf[num_read + 1] = 0;  // null terminate the string
+
+    total_num_read = num_read;
+    return success;
+}
+
+SerialDevice::SerialDevice(const char *serial_port, int baud_rate)
+{
+    is_open_ = false;
+    serial_port_filename_ = serial_port;
+    baud_rate_ = baud_rate;
+    sleep_time_between_each_char_ = 5; // ms
+    bool ok = this->openDevice();
+}
+
+SerialDevice::~SerialDevice()
+{
+    if (isOpen())
+        closeDevice();
+}
+
+bool SerialDevice::isOpen() const
+{
+    return is_open_;
+}
+
+bool SerialDevice::closeDevice()
+{
+    if (isOpen())
+    {
+        int result = close(fd_);
+        if (result == 0)
+        {
+            return true;
+        }
+        else
+        {
+            std::ostringstream os;
+            os << "SerialDevice." << __FUNCTION__ << ": error closing serial device. TODO: see errno";
+            tempi::Logger::log(ERROR, os);
+            return false;
+        }
+    }
+    else
+    {
+        std::ostringstream os;
+        os << "SerialDevice." << __FUNCTION__ << ": Device is not open";
+        tempi::Logger::log(ERROR, os);
+        return false;
+    }
+}
+
+bool SerialDevice::openDevice()
+{
+    if (this->isOpen())
+    {
+        this->closeDevice();
+    }
+    bool ok = this->init_serialport(serial_port_filename_.c_str(), baud_rate_);
+    this->is_open_ = ok;
+    if (ok)
+    {
+        if (Logger::isEnabledFor(NOTICE))
+        {
+            std::ostringstream os;
+            os << "SerialDevice." << __FUNCTION__ << ": successfully opened " << serial_port_filename_ << " at " << baud_rate_;
+            tempi::Logger::log(NOTICE, os);
+        }
+    }
+    else
+    {
+        std::ostringstream os;
+        os << "SerialDevice." << __FUNCTION__ << ": failure";
+        tempi::Logger::log(ERROR, os);
+    }
+}
+
+/**
+ * Takes the string name of the serial port (e.g. "/dev/tty.usbserial","COM1")
+ * and a baud rate (BPS) and connects to that port at that speed and 8N1.
+ * opens the port in fully raw mode so you can send binary data.
+ * returns success or not
+ */
+bool SerialDevice::init_serialport(const char* serialport, int baud)
+{
+    struct termios toptions;
+    //fprintf(stderr,"init_serialport: opening port %s @ %d bps\n",
+    //        serialport,baud);
+    fd_ = open(serialport, O_RDWR | O_NOCTTY | O_NDELAY);
+    if (fd_ == -1)
+    {
+        perror("init_serialport: Unable to open port ");
+        return false;
+    }
+    if (tcgetattr(fd_, &toptions) < 0)
+    {
+        perror("init_serialport: Couldn't get term attributes");
+        return false;
+    }
+    speed_t brate = baud; // let you override switch below if needed
+    switch (baud)
+    {
+        case 4800:   brate = B4800;   break;
+        case 9600:   brate = B9600;   break;
+// #ifndef OSNAME_LINUX
+//         case 14400:  brate = B14400;  break;
+// #endif
+        case 19200:  brate = B19200;  break;
+// #ifndef OSNAME_LINUX
+//         case 28800:  brate = B28800;  break;
+// #endif
+        case 38400:  brate = B38400;  break;
+        case 57600:  brate = B57600;  break;
+        case 115200: brate = B115200; break;
+    }
+    cfsetispeed(&toptions, brate);
+    cfsetospeed(&toptions, brate);
+
+    // 8N1
+    toptions.c_cflag &= ~PARENB;
+    toptions.c_cflag &= ~CSTOPB;
+    toptions.c_cflag &= ~CSIZE;
+    toptions.c_cflag |= CS8;
+    // no flow control
+    toptions.c_cflag &= ~CRTSCTS;
+
+    toptions.c_cflag |= CREAD | CLOCAL;  // turn on READ & ignore ctrl lines
+    toptions.c_iflag &= ~(IXON | IXOFF | IXANY); // turn off s/w flow ctrl
+
+    toptions.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); // make raw
+    toptions.c_oflag &= ~OPOST; // make raw
+
+    // see: http://unixwiz.net/techtips/termios-vmin-vtime.html
+    toptions.c_cc[VMIN]  = 0;
+    toptions.c_cc[VTIME] = 20;
+    
+    if (tcsetattr(fd_, TCSANOW, &toptions) < 0)
+    {
+        perror("init_serialport: Couldn't set term attributes");
+        return false;
+    }
+    return true;
+}
+
---
+++ plugins-base/serial/serialdevicenode.cpp
@@ -0,0 +1,162 @@
+/*
+ * Copyright (C) 2011 Alexandre Quessy
+ * Copyright (C) 2011 Michal Seta
+ * Copyright (C) 2012 Nicolas Bouillot
+ * Copyright (C) 2012 Emmanuel Durand
+ *
+ * This file is part of Tempi-plugins-base.
+ *
+ * This program is free software; you can redistither version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Tempi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Tempi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "plugins-base/serial/serialdevicenode.h"
+#include "tempi/utils.h"
+#include "tempi/log.h"
+#include <iostream>
+
+namespace tempi {
+namespace plugins_base {
+
+const char * const SerialDeviceNode::DATA_INLET = "data";
+const char * const SerialDeviceNode::DATA_OUTLET = "data";
+const char * const SerialDeviceNode::IS_OPEN_ATTR = "is_open";
+const char * const SerialDeviceNode::BAUD_RATE_ATTR = "baud_rate";
+const char * const SerialDeviceNode::DEVICE_FILE_ATTR = "device";
+
+SerialDeviceNode::SerialDeviceNode() :
+    Node()
+{
+    setShortDocumentation("Communicates with serial devices.");
+    setLongDocumentation("Right now, it only supports 8N1 without flow control. It opens the device when you set it to a valid file name.");
+
+    // File name
+    addAttribute(Attribute::ptr(new Attribute(DEVICE_FILE_ATTR, Message("s", ""), "UNIX-like device file name. example: /dev/ttyUSB0")));
+    // Baud rate
+    addAttribute(Attribute::ptr(new Attribute(BAUD_RATE_ATTR, Message("i", 9600), "Baud rate. Valid values are 4800, 9600, 19200, 38400, 57600, 115200.")));
+    addAttribute(Attribute::ptr(new Attribute(IS_OPEN_ATTR, Message("b", false), "Tells you whether the communication is open or not.")));
+    getAttribute(IS_OPEN_ATTR)->setMutable(false);
+
+    addInlet(DATA_INLET, "Messages to send to the device. Must be a single blob.");
+    addOutlet(DATA_OUTLET, "Messages received from the device. Single blobs.");
+}
+
+void SerialDeviceNode::processMessage(const char *inlet, const Message &message)
+{
+    if (utils::stringsMatch(DATA_INLET, inlet))
+    {
+        if (device_.get() == 0)
+        {
+            std::ostringstream os;
+            os << "SerialDeviceNode::" << __FUNCTION__ << ": Device file has not been specified.";
+            Logger::log(ERROR, os);
+        }
+        else if (message.typesMatch("B"))
+        {
+            atom::BlobValue::ptr blob = message.getBlob(0);
+            device_->writeBlob(blob->getValue(), blob->getSize());
+        }
+        else if (message.typesMatch("s"))
+        {
+            std::string string = message.getString(0);
+            device_->writeBlob(string.c_str(), string.size() + 1);
+        }
+        else
+        {
+            std::ostringstream os;
+            os << "SerialDeviceNode::" << __FUNCTION__ << ": Message must be a blob."; // or a string for now
+            Logger::log(ERROR, os);
+        }
+    }
+}
+
+bool SerialDeviceNode::onNodeAttributeChanged(const char *name, const Message &value)
+{
+    //std::cout << "SerialDeviceNode::" << __FUNCTION__ << "(" << name << ", " << value << ")" << std::endl;
+    if (utils::stringsMatch(DEVICE_FILE_ATTR, name))
+    {
+        // TODO: do not open it twice
+        std::string device = value.getString(0);
+        if (device == "")
+        {
+            this->device_.reset();
+            this->setAttributeValue(IS_OPEN_ATTR, Message("b", false));
+            return true;
+        }
+        if (Logger::isEnabledFor(NOTICE))
+        {
+            std::ostringstream os;
+            os << "SerialDeviceNode: Change serial device to " << device;
+            Logger::log(NOTICE, os);
+        }
+        // TODO: validate that file name exists
+        this->device_.reset(new SerialDevice(
+            device.c_str(),
+            this->getAttributeValue(BAUD_RATE_ATTR).getInt(0)));
+        bool success = this->device_->openDevice();
+        this->setAttributeValue(IS_OPEN_ATTR, Message("b", success));
+        return true;
+    }
+    else if (utils::stringsMatch(name, BAUD_RATE_ATTR))
+    {
+        int rate = value.getInt(0);
+        // validate baud rate
+        switch (rate)
+        {
+            case 4800:
+            case 9600:
+            case 19200:
+            case 38400:
+            case 57600:
+            case 115200:
+                break;
+            default:
+                return false;
+        }
+        if (Logger::isEnabledFor(NOTICE))
+        {
+            std::ostringstream os;
+            os << "SerialDeviceNode: Change baud rate to " << rate;
+            Logger::log(NOTICE, os);
+        }
+    }
+    return true;
+}
+
+void SerialDeviceNode::doTick()
+{
+    if (this->device_ && this->device_->isOpen())
+    {
+        size_t max_length = 256;
+        char result[256];
+        memset(result, 0, 256);
+        size_t total_num_read;
+        unsigned long long timeout_ms = 50;
+        // TODO: do not use until_char, but simply output it all as a blob
+        static const char until_char = '\n';
+        bool use_until_char = true;
+        // TODO: use a mutex!
+        bool did_read_some = this->device_->readUntil(result, max_length, total_num_read, timeout_ms, until_char, use_until_char);
+        if (did_read_some)
+        {
+            // TODO: output a blob, not a string
+            // we remove the trailing new line
+            std::string tmp_string = std::string(result);
+            Message to_output_message;
+            to_output_message.appendString(tmp_string.c_str()); //tmp_string.substr(0, total_num_read - 1).c_str());
+            output(DATA_OUTLET, to_output_message);
+        }
+    }
+}
+
+} // end of namespace
+} // end of namespace
+
