58deb5c17c6c71be8db59e96e5b2c9eb0d064e2c
Alexandre Quessy <alexandre@quessy.net>
2013-02-13 15:33:42 -0500

    arduino firmware: poll. Read pot1 and pot2 as well

--- prototypes/serial/arduino-code/tempi_kit_01/tempi_kit_01.ino
+++ prototypes/serial/arduino-code/tempi_kit_01/tempi_kit_01.ino
@@ -3,46 +3,101 @@
 
 Messenger message = Messenger();
 TKPotentiometer pot_I0(I0);
+TKPotentiometer pot_I1(I1);
+TKPotentiometer pot_I2(I2);
 TKLed led_O0(O0);
 TKLed led_O1(O1);
 TKLed led_O2(O2);
-static const unsigned long interval_hello = 2000;
-unsigned long last_hello_time = 0;
+unsigned long interval_poll = 50;
+unsigned long last_poll_time = 0;
+boolean is_polling = false;
+
+void send_ok(const char *command)
+{
+  Serial.print("ok ");
+  Serial.println(command);
+  Serial.flush();
+}
+
+void send_read_values()
+{
+  // I0
+  Serial.print("I0");
+  Serial.print(" ");
+  Serial.print(pot_I0.get(), DEC); // [0-1023]
+  Serial.println();
+  // I1
+  Serial.print("I1");
+  Serial.print(" ");
+  Serial.print(pot_I1.get(), DEC); // [0-1023]
+  Serial.println();
+  // I2
+  Serial.print("I2");
+  Serial.print(" ");
+  Serial.print(pot_I2.get(), DEC); // [0-1023]
+  Serial.println();
+  // TODO: other ones
+  Serial.flush();
+}
 
 void handleMessage()
 {
-  if (message.checkString("r"))
+  if (message.checkString("start"))
+  { // start polling
+    is_polling = true;
+    send_ok("start");
+  }
+  else if (message.checkString("stop"))
+  { // stop polling
+    is_polling = false;
+    send_ok("stop");
+  }
+  else if (message.checkString("interval"))
+  {
+    int value = message.readInt();
+    if (value < 0 || value > 100000)
+    {
+      Serial.println("error out_of_range");
+      Serial.flush();
+    }
+    else
+    {
+      interval_poll = (unsigned int) value;
+      send_ok("interval");
+    }
+  }
+  else if (message.checkString("r"))
   { // Read
-      Serial.print("I0");
-      Serial.print(" ");
-      Serial.print(pot_I0.get(), DEC); // [0-1023]
-      Serial.println();
+    send_read_values();
   }
   else if (message.checkString("led0"))
   {
     int value = message.readInt();
     led_O0.brightness(value); // [0-1023]
-    Serial.println("success");
+    send_ok("led0");
   }
   else if (message.checkString("led1"))
   {
     int value = message.readInt();
     led_O1.brightness(value); // [0-1023]
+    send_ok("led1");
   }
   else if (message.checkString("led2"))
   {
     int value = message.readInt();
     led_O2.brightness(value); // [0-1023]
+    send_ok("led2");
   }
   else if (message.checkString("ping")) // ping
   {
     Serial.println("pong");
+    Serial.flush();
   }
   else
   {
     Serial.println("error no such handler");
+    Serial.flush();
   }
-  Serial.flush();
 }
 
 void setup()
@@ -60,11 +115,13 @@ void loop()
   {
     message.process(Serial.read());
   }
-  //if ((millis() - last_hello_time) > interval_hello)
-  //{
-    //Serial.println("hello");
-  //  last_hello_time = millis();
-  //  Serial.flush();
-  //}
+  if (is_polling)
+  {
+    if ((millis() - last_poll_time) > interval_poll)
+    {
+      last_poll_time = millis();
+      send_read_values();
+    }
+  }
 }
 
